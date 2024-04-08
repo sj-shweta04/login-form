bb1c7e1d2fd68b91575dbb8eab7ccbb8937ae452
shweta_jadhav <sj.shweta014@gmail.com>
2024-04-08 22:57:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import StandardForm from './compoents/StandardForm';
+
+function App() {
+  return (
+    <div className="App">
+     <StandardForm/>
+    </div>
+  );
+}
+
+export default App;
